cfeeeb19351d26a7da2053144072fee598bff9a6
lamthaithuy01 <103127697+lamthaithuy01@users.noreply.github.com>
2022-04-24 23:41:20 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+import info from "./calculator.js";
+import { calculateCircleArea } from "./calculator.js";
+import { calculateRectArea } from "./calculator.js";
+calculateCircleArea(30);
+calculateRectArea(10,5);
+console.log(info.name);
